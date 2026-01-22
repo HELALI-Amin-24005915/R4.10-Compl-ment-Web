f4908a002c9c469c72bd9357cefe967ef12f4269
HELALI <amin.helali@etu.univ-amu.fr>
2026-01-22 16:56:59 +0100

    3 - DOM : modification du contenu d’un noeud

--- TD1/td1.js
+++ TD1/td1.js
@@ -8,16 +8,20 @@ function getRandomArbitrary() {
     for (let I_i = 0; I_i < 20; I_i++){
         A_array.push(Math.floor(Math.random() * (I_max - I_min) + I_min))
     }
-    return A_array;
 }
-console.log(getRandomArbitrary());
 //Q2
 
-function Affichage(){
+function affichage(){
     var O_el = document.getElementById("affichage-temperature");
     O_el.textContent = A_array[I_I];
     I_I++;
-}
 
-Affichage();
+    if(I_I >= A_array.length){
+        I_I = 0;
+    }
+}
+const I_timer = setInterval(affichage, 2000);
+
+getRandomArbitrary();
+affichage();
 
