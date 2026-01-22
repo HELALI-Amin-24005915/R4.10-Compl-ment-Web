16392ebad4b9e26754acc1f6c6200367665a5316
HELALI <amin.helali@etu.univ-amu.fr>
2026-01-22 16:32:40 +0100

    2 - HTML

--- td1.js
+++ td1.js
@@ -1,15 +1,23 @@
-function getRandomArbitrary(min, max) {
-    const A_array = [];
-    min = -10;
-    max = 40;
-    for (i = 0; i < 20; i++){
-        A_array.push(Math.floor(Math.random() * (max - min) + min))
+//Q1
+var I_I = 0;
+var A_array = [];
+
+function getRandomArbitrary() {
+    let I_min = -10;
+    let I_max = 40;
+    for (let I_i = 0; I_i < 20; I_i++){
+        A_array.push(Math.floor(Math.random() * (I_max - I_min) + I_min))
     }
     return A_array;
 }
 console.log(getRandomArbitrary());
+//Q2
 
+function Affichage(){
+    var O_el = document.getElementById("affichage-temperature");
+    O_el.textContent = A_array[I_I];
+    I_I++;
+}
 
-
-
+Affichage();
 
